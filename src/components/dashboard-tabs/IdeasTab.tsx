4dd17043b4fe569364ad348c11f4549903d0ba1d
bigimpactkartik <bigimpact.kartik@gmail.com>
2025-07-29 16:28:03 +0530

    Updated IdeasTab.tsx

--- src/components/dashboard-tabs/IdeasTab.tsx
+++ src/components/dashboard-tabs/IdeasTab.tsx
@@ -493,7 +493,7 @@ const IdeasTab = () => {
                 <TableHead className="text-white">Status</TableHead>
                 <TableHead className="text-white">Priority</TableHead>
                 <TableHead className="text-white">Created</TableHead>
-                <TableHead className="text-white">Caregory </TableHead>
+                <TableHead className="text-white">Category </TableHead>
                 <TableHead className="text-white">Actions</TableHead>
               </TableRow>
             </TableHeader>
